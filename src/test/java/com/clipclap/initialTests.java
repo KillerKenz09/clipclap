00fc9dc9a8b70a3cd173064016671797bca41eff
Richard Guy <Richard.Guy@edelman.com>
2014-09-15 15:27:43 +0100

    ...

---
+++ src/test/java/com/clipclap/initialTests.java
@@ -0,0 +1,14 @@
+package com.clipclap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class initialTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
